8cc9c889c143ecf5913349966489c26c5494ddf8
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-11-04 13:44:40 -0700

    added calucate winner helper function. Checks squares in each row of board as a subarray. If all indeces in subarray contain equal values (3 of same val in a row) returns that value (X or O) as winner

--- src/index.js
+++ src/index.js
@@ -28,13 +28,14 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice(); //this just makes a copy of the squares array
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
 
   renderSquare(i) {
@@ -49,7 +50,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
 
     return (
       <div>
@@ -94,3 +95,24 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
+
+// helper function to calculate winner
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
